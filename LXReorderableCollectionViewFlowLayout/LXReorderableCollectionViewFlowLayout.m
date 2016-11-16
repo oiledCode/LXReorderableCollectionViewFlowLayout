3afbcdb8c54e1a08129d88ca21dd6a9056588f3d
Elio D'antoni <elio@qoqa.com>
2016-11-16 13:16:57 +0100

    change transform and alpha value

--- LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
+++ LXReorderableCollectionViewFlowLayout/LXReorderableCollectionViewFlowLayout.m
@@ -347,7 +347,8 @@ static NSString * const kLXCollectionViewKeyPath = @"collectionView";
              animations:^{
                  __strong typeof(self) strongSelf = weakSelf;
                  if (strongSelf) {
-                     strongSelf.currentView.transform = CGAffineTransformMakeScale(0.9f, 0.9f);
+                     strongSelf.currentView.transform = CGAffineTransformMakeScale(0.95f, 0.95f);
+                     strongSelf.currentView.alpha = 0.9
                      highlightedImageView.alpha = 0.0f;
                      imageView.alpha = 1.0f;
                  }
@@ -386,6 +387,7 @@ static NSString * const kLXCollectionViewKeyPath = @"collectionView";
                      __strong typeof(self) strongSelf = weakSelf;
                      if (strongSelf) {
                          strongSelf.currentView.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
+                         strongSelf.currentView.alpha = 1.0
                          strongSelf.currentView.center = layoutAttributes.center;
                      }
                  }
